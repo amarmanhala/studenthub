7e881997958ecc106d3b419a657ff21eb98f82d2
Amarpreet Singh <amar.manhala@gmail.com>
2024-04-30 00:34:01 -0400

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Access the students array from the JSON data
-    const students = data.students;
+import { data } from "./data.json";
 
-    // Output student information
-    students.forEach(student => {
-      console.log(`Name: ${student.name}, Age: ${student.age}`);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+console.log(data.age);
